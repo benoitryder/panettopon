5a6e5b401930050a88ef3a844b063643527c0429
Benoît Ryder <benoit@ryder.fr>
2011-06-13 15:41:33 +0200

    gui, ScreenMenu: move focus with arrow keys

--- src/gui/screen_menu.cpp
+++ src/gui/screen_menu.cpp
@@ -23,6 +23,28 @@ bool ScreenMenu::onInputEvent(const sf::Event &ev)
       return true;
     }
   }
+
+  if( ev.Type == sf::Event::KeyPressed ) {
+    // move focus
+    if( focused_ ) {
+      Widget::Neighbor n;
+      switch( ev.Key.Code ) {
+        case sf::Key::Up:    n = Widget::NEIGHBOR_UP;    break;
+        case sf::Key::Down:  n = Widget::NEIGHBOR_DOWN;  break;
+        case sf::Key::Left:  n = Widget::NEIGHBOR_LEFT;  break;
+        case sf::Key::Right: n = Widget::NEIGHBOR_RIGHT; break;
+        default: n = Widget::NEIGHBOR_NONE; break;
+      }
+      if( n != Widget::NEIGHBOR_NONE ) {
+        Widget *next_focused = focused_->neighbor(n);
+        if( next_focused ) {
+          this->focus(next_focused);
+          return true;
+        }
+      }
+    }
+  }
+
   return false;
 }
 
