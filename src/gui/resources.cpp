236b352c5dbdb6b6c38db30a6f5a0cd0049acc23
Benoît Ryder <benoit@ryder.fr>
2011-06-30 21:25:11 +0200

    gui: add ImageTile and ResField classes

--- src/gui/resources.cpp
+++ src/gui/resources.cpp
@@ -1,5 +1,7 @@
 #include <cassert>
 #include <stdexcept>
+#include <SFML/Graphics/Renderer.hpp>
+#include <SFML/Graphics/Sprite.hpp>
 #include "resources.h"
 
 namespace gui {
@@ -49,6 +51,117 @@ const sf::Image *ResourceManager::getImage(const std::string &name)
   images_[name] = pimg;
 
   return pimg;
+}
+
+
+
+ImageTile::ImageTile():
+    image_(NULL)
+{
+}
+
+void ImageTile::create(const sf::Image *img, const sf::IntRect &rect)
+{
+  image_ = img;
+  rect_ = rect;
+}
+
+void ImageTile::create(const sf::Image *img, int sx, int sy, int x, int y)
+{
+  assert( img->GetWidth() % sx == 0 );
+  assert( img->GetHeight() % sy == 0 );
+  const int kx = img->GetWidth()  / sx;
+  const int ky = img->GetHeight() / sy;
+  this->create(img, sf::IntRect(kx*x, ky*y, kx, ky));
+}
+
+void ImageTile::render(sf::Renderer &renderer, float x, float y, float w, float h) const
+{
+  sf::FloatRect coords = image_->GetTexCoords(rect_);
+  const float left   = coords.Left;
+  const float top    = coords.Top;
+  const float right  = coords.Left + coords.Width;
+  const float bottom = coords.Top  + coords.Height;
+
+  renderer.SetTexture(image_);
+  renderer.Begin(sf::Renderer::TriangleStrip);
+    renderer.AddVertex(x,   y,   left,  bottom);
+    renderer.AddVertex(x+w, y,   right, bottom);
+    renderer.AddVertex(x,   y+h, left,  top);
+    renderer.AddVertex(x+w, y+h, right, top);
+  renderer.End();
+}
+
+void ImageTile::setToSprite(sf::Sprite *spr, bool center) const
+{
+  spr->SetImage(*image_);
+  spr->SetSubRect(rect_);
+  if( center ) {
+    spr->SetOrigin( rect_.Width/2., rect_.Height/2. );
+  }
+}
+
+
+
+ResField::ResField():
+    bk_size(0), img_field_frame(NULL)
+{
+}
+
+void ResField::load(ResourceManager *res_mgr)
+{
+  const sf::Image *img;
+
+  // Block tiles (and block size)
+  img = res_mgr->getImage("BkColor-map");
+  bk_size = img->GetHeight()/5;
+  int nb_colors = img->GetWidth()/bk_size;
+  tiles_bk_color.resize(nb_colors); // create sprites, uninitialized
+  for(int i=0; i<nb_colors; i++ ) {
+    TilesBkColor &tiles = tiles_bk_color[i];
+    tiles.normal.create(img, nb_colors, 5, i, 0);
+    tiles.bg    .create(img, nb_colors, 5, i, 1);
+    tiles.face  .create(img, nb_colors, 5, i, 2);
+    tiles.flash .create(img, nb_colors, 5, i, 3);
+    tiles.mutate.create(img, nb_colors, 5, i, 4);
+  }
+
+  // Garbages
+  img = res_mgr->getImage("BkGarbage-map");
+  for(int x=0; x<4; x++) {
+    for(int y=0; y<4; y++) {
+      tiles_gb.tiles[x][y].create(img, 8, 4, x, y);
+    }
+  }
+  //XXX center: setRepeat(true)
+  for(int x=0; x<2; x++) {
+    for(int y=0; y<2; y++) {
+      tiles_gb.center[x][y].create(img, 8, 4, 4+x, y);
+    }
+  }
+  tiles_gb.mutate.create(img, 4, 2, 3, 0);
+  tiles_gb.flash .create(img, 4, 2, 3, 1);
+
+  // Frame
+  img_field_frame = res_mgr->getImage("Field-Frame");
+
+  // Cursor
+  img = res_mgr->getImage("SwapCursor");
+  tiles_cursor[0].create(img, 1, 2, 0, 0);
+  tiles_cursor[1].create(img, 1, 2, 0, 1);
+
+  // Labels
+  img = res_mgr->getImage("Labels");
+  tiles_labels.combo.create(img, 2, 1, 0, 0);
+  tiles_labels.chain.create(img, 2, 1, 1, 0);
+
+  // Waiting garbages
+  img = res_mgr->getImage("GbWaiting-map");
+  const size_t gb_waiting_sx = FIELD_WIDTH/2; // on 2 rows
+  for(int i=0; i<FIELD_WIDTH; i++) {
+    tiles_gb_waiting.blocks[i].create(img, gb_waiting_sx+1, 2, i%gb_waiting_sx, i/gb_waiting_sx);
+  }
+  tiles_gb_waiting.line.create(img, gb_waiting_sx+1, 2, gb_waiting_sx, 0);
 }
 
 
--- src/gui/resources.h
+++ src/gui/resources.h
@@ -4,6 +4,11 @@
 #include <map>
 #include <string>
 #include <SFML/Graphics/Image.hpp>
+#include "../util.h"
+
+namespace sf {
+  class Sprite;
+}
 
 namespace gui {
 
@@ -23,6 +28,79 @@ class ResourceManager
 
   typedef std::map<std::string, sf::ResourcePtr<sf::Image> > ImageContainer;
   ImageContainer images_;  ///< Loaded images.
+};
+
+
+/** @brief Image subpart with rendering capabilities
+ *
+ * Similar to sf::Sprite but without positioning, scaling, rotating, etc.
+ *
+ * Tiles are drawn using positions in blocks and assuming Y=0 is the bottom of
+ * the screen. It used in particular for field blocks.
+ */
+class ImageTile
+{
+ public:
+  ImageTile();
+
+  /// Initialize the tile using image subrect
+  void create(const sf::Image *img, const sf::IntRect &rect);
+  /// Initialize the tile (x,y) from a (sx,sy) tilemap
+  void create(const sf::Image *img, int sx, int sy, int x, int y);
+  /// Draw the tile at given position
+  void render(sf::Renderer &renderer, float x, float y, float w, float h) const;
+  /** @brief Set the tile on a sprite
+   *
+   * If \e center is \e true, sprite's origin is set to be centred on the tile.
+   */
+  void setToSprite(sf::Sprite *spr, bool center=false) const;
+
+ private:
+  const sf::Image *image_;
+  sf::IntRect rect_;
+};
+
+
+/// Container for field display resources
+class ResField
+{
+ public:
+  ResField();
+  void load(ResourceManager *res_mgr);
+
+  /// Block pixel size
+  unsigned int bk_size;
+
+  /// Tile group for block of a given color
+  struct TilesBkColor {
+    ImageTile normal, bg, face, flash, mutate;
+  };
+  /// Color blocks
+  std::vector<TilesBkColor> tiles_bk_color;
+
+  /// Garbages
+  struct TilesGb {
+    ImageTile tiles[4][4];
+    ImageTile center[2][2];
+    ImageTile mutate, flash;
+  } tiles_gb;
+
+  /// Field frame
+  const sf::Image *img_field_frame;
+
+  /// Cursor (two positions)
+  ImageTile tiles_cursor[2];
+
+  /// Labels (combo and chain)
+  struct TilesLabels {
+    ImageTile combo, chain;
+  } tiles_labels;
+
+  /// Waiting garbages
+  struct TilesGbWaiting {
+    ImageTile blocks[FIELD_WIDTH];
+    ImageTile line;
+  } tiles_gb_waiting;
 };
 
 
