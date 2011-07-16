1140b22f81b87d22071e98fb16be5eca1329a821
Benoît Ryder <benoit@ryder.fr>
2011-07-16 20:51:43 +0200

    Add global try/catch to main()

--- src/main.cpp
+++ src/main.cpp
@@ -71,138 +71,143 @@ void usage(void)
  */
 int main(int /*argc*/, char **argv)
 {
-  OptGetItem opts[] = {
-    { 'c', "conf", OPTGET_STR, {} },
-    { 'i', "interface", OPTGET_STR, {} },
-    { 'n', "nick", OPTGET_STR, {} },
-    { 'o', "log-file", OPTGET_STR, {} },
-    { 'h', "help", OPTGET_FLAG, {} },
-    { 0, 0, OPTGET_NONE, {} }
-  };
+  try {
+    OptGetItem opts[] = {
+      { 'c', "conf", OPTGET_STR, {} },
+      { 'i', "interface", OPTGET_STR, {} },
+      { 'n', "nick", OPTGET_STR, {} },
+      { 'o', "log-file", OPTGET_STR, {} },
+      { 'h', "help", OPTGET_FLAG, {} },
+      { 0, 0, OPTGET_NONE, {} }
+    };
 
-  FileLogger *logger = new FileLogger;
-  Logger::setLogger(logger);
+    FileLogger *logger = new FileLogger;
+    Logger::setLogger(logger);
 
 
-  // Program arguments
-  const char *conf_file = NULL;
-  char *port = NULL;
-  char *host = NULL;
-  const char *nick = NULL;
-  const char *intfarg = NULL;
+    // Program arguments
+    const char *conf_file = NULL;
+    char *port = NULL;
+    char *host = NULL;
+    const char *nick = NULL;
+    const char *intfarg = NULL;
 
-  char *const *opt_args = argv+1;
-  OptGetItem *opt;
-  int ret;
-  for(;;) {
-    ret = optget_parse(opts, &opt_args, &opt);
-    if( ret != OPTGET_OK )
-      break;
+    char *const *opt_args = argv+1;
+    OptGetItem *opt;
+    int ret;
+    for(;;) {
+      ret = optget_parse(opts, &opt_args, &opt);
+      if( ret != OPTGET_OK )
+        break;
 
-    // extra arguments
-    if( opt->type == OPTGET_NONE ) {
-      if( port == NULL ) {
-        port = opt->value.str;
-      } else if( host == NULL ) {
-        host = port;
-        port = opt->value.str;
+      // extra arguments
+      if( opt->type == OPTGET_NONE ) {
+        if( port == NULL ) {
+          port = opt->value.str;
+        } else if( host == NULL ) {
+          host = port;
+          port = opt->value.str;
+        }
+        else {
+          LOG("unexpected extra argument: %s", opt->value.str);
+        }
+      } else {
+        // options
+        switch( opt->short_name ) {
+          case 'c':
+            conf_file = opt->value.str;
+          case 'i':
+            intfarg = opt->value.str;
+            break;
+          case 'n':
+            nick = opt->value.str;
+            break;
+          case 'o':
+            logger->setFile( opt->value.str );
+            break;
+          case 'h':
+            usage();
+            return 0;
+            break;
+          default:
+            break;
+        }
       }
-      else {
-        LOG("unexpected extra argument: %s", opt->value.str);
-      }
-    } else {
-      // options
-      switch( opt->short_name ) {
-        case 'c':
-          conf_file = opt->value.str;
-        case 'i':
-          intfarg = opt->value.str;
+    }
+
+    // standard parsing errors
+    if( ret != OPTGET_LAST ) {
+      switch( ret ) {
+        case OPTGET_ERR_SHORT_NAME:
+        case OPTGET_ERR_LONG_NAME:
+          LOG("unknown option: %s", *opt_args);
           break;
-        case 'n':
-          nick = opt->value.str;
+        case OPTGET_ERR_VAL_FMT:
+          LOG("invalid option value: %s", *opt_args);
           break;
-        case 'o':
-          logger->setFile( opt->value.str );
+        case OPTGET_ERR_VAL_MISSING:
+          LOG("missing option value: -%c", opt->short_name);
           break;
-        case 'h':
-          usage();
-          return 0;
+        case OPTGET_ERR_VAL_UNEXP:
+          LOG("unexpected value for option: %s", *opt_args);
           break;
         default:
+          // nothing to do
           break;
       }
+      return 2;
     }
-  }
 
-  // standard parsing errors
-  if( ret != OPTGET_LAST ) {
-    switch( ret ) {
-      case OPTGET_ERR_SHORT_NAME:
-      case OPTGET_ERR_LONG_NAME:
-        LOG("unknown option: %s", *opt_args);
-        break;
-      case OPTGET_ERR_VAL_FMT:
-        LOG("invalid option value: %s", *opt_args);
-        break;
-      case OPTGET_ERR_VAL_MISSING:
-        LOG("missing option value: -%c", opt->short_name);
-        break;
-      case OPTGET_ERR_VAL_UNEXP:
-        LOG("unexpected value for option: %s", *opt_args);
-        break;
-      default:
-        // nothing to do
-        break;
+    // load configuration
+    IniFile cfg;
+    if( conf_file == NULL && ::access(CONF_FILE_DEFAULT, R_OK) == 0 )
+      conf_file = CONF_FILE_DEFAULT; // default config file
+    if( conf_file != NULL ) {
+      if( !cfg.load(conf_file) ) {
+        LOG("failed to load configuration file");
+        return 3;
+      }
     }
-    return 2;
-  }
 
-  // load configuration
-  IniFile cfg;
-  if( conf_file == NULL && ::access(CONF_FILE_DEFAULT, R_OK) == 0 )
-    conf_file = CONF_FILE_DEFAULT; // default config file
-  if( conf_file != NULL ) {
-    if( !cfg.load(conf_file) ) {
-      LOG("failed to load configuration file");
-      return 3;
-    }
-  }
+    if( intfarg != NULL )
+      cfg.set("Global", "Interface", intfarg);
+    if( port != NULL )
+      cfg.set("Global", "Port", port);
+    if( host != NULL )
+      cfg.set("Client", "Hostname", host);
+    if( nick != NULL )
+      cfg.set("Client", "Nick", nick);
 
-  if( intfarg != NULL )
-    cfg.set("Global", "Interface", intfarg);
-  if( port != NULL )
-    cfg.set("Global", "Port", port);
-  if( host != NULL )
-    cfg.set("Client", "Hostname", host);
-  if( nick != NULL )
-    cfg.set("Client", "Nick", nick);
+    // init randomness
+    ::srand( game_time() );
 
-  // init randomness
-  ::srand( game_time() );
-
-  const std::string intfstr = cfg.get("Global", "Interface", "server");
+    const std::string intfstr = cfg.get("Global", "Interface", "server");
 #ifndef WITHOUT_INTF_SERVER
-  if( intfstr == "server" ) {
-    BasicServerInterface intf;
-    ret = intf.run(cfg);
-  } else
+    if( intfstr == "server" ) {
+      BasicServerInterface intf;
+      ret = intf.run(cfg);
+    } else
 #endif
 #ifndef WITHOUT_INTF_CURSES
-  if( intfstr == "curses" ) {
-    curses::CursesInterface intf;
-    ret = intf.run(cfg);
-  } else
+    if( intfstr == "curses" ) {
+      curses::CursesInterface intf;
+      ret = intf.run(cfg);
+    } else
 #endif
 #ifndef WITHOUT_INTF_GUI
-  if( intfstr == "gui" ) {
-    gui::GuiInterface intf;
-    ret = intf.run(cfg);
-  } else
+    if( intfstr == "gui" ) {
+      gui::GuiInterface intf;
+      ret = intf.run(cfg);
+    } else
 #endif
-  {
-    LOG("invalid interface: '%s'", intfstr.c_str());
-    return 2;
+    {
+      LOG("invalid interface: '%s'", intfstr.c_str());
+      return 2;
+    }
+    return ret ? 0 : 1;
+  } catch(const std::exception &e) {
+    fprintf(stderr, "fatal error: %s\n", e.what());
+    return 1;
   }
-  return ret ? 0 : 1;
 }
 
